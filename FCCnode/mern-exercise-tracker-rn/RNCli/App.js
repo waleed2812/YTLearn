4cf056a3431dc5c6830738c49f42a7c68cdd9b98
Waleed Butt <56509403+waleedbutt98@users.noreply.github.com>
2021-04-11 07:58:40 +0500

    Drawer Navigation Done

--- FCCnode/mern-exercise-tracker-rn/RNCli/App.js
+++ FCCnode/mern-exercise-tracker-rn/RNCli/App.js
@@ -1,29 +1,91 @@
 
-import React from 'react';
+import * as React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   Text,
   useColorScheme,
   View,
+  useWindowDimensions,
 } from 'react-native';
 
+import 'react-native-gesture-handler';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import Colors from './constants/Colors';
 import styles from './constants/style';
 
+// Drawer Object for Drawer Screens
+const Drawer = createDrawerNavigator();
+
+const Screen = ({navigation}) => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Screen 1</Text>
+    </View>
+    );
+}
+
+// Drawer Navigator for Countries and World
+const MyDrawer = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName= 'Excercises'
+      // drawerStyle={styles.drawer}
+      drawerType={useWindowDimensions().width >= 768 ? 'permanent' : 'front'}
+      screenOptions={{
+        activeTintColor: Colors.darker,
+        inactiveTintColor: Colors.dark,
+        headerTitle:'Excercise Tracker',
+        // labelStyle: styles.drawerItem,
+      }}
+      >
+      <Drawer.Screen
+        name={'Excercises'}
+        component={Screen}
+        // options={{
+        //   drawerIcon: ({color}) => (
+        //     <Ionicons name={'earth'} size={20} color={color} />
+        //   ),
+        // }}
+      />
+      <Drawer.Screen
+        name={'Create Excercises'}
+        component={Screen}
+        // options={{
+        //   drawerIcon: ({color}) => (
+        //     <Ionicons name={'flag'} size={20} color={color} />
+        //   ),
+        // }}
+      />
+      <Drawer.Screen
+        name={'Create User'}
+        component={Screen}
+        // options={{
+        //   drawerIcon: ({color}) => (
+        //     <Ionicons name={'heart'} size={20} color={color} />
+        //   ),
+        // }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+
 const App = () => {
 
   return (
-    <SafeAreaView>
-      <StatusBar  />
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <View>
-            <Text>Hello! World. </Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <StatusBar
+        translucent={false}
+        backgroundColor={Colors.light}
+        barStyle={'dark-content'}
+      />
+      <NavigationContainer>
+        <MyDrawer />
+      </NavigationContainer>
+    </>
   );
 };
 
--- FCCnode/mern-exercise-tracker-rn/RNCli/constants/style.js
+++ FCCnode/mern-exercise-tracker-rn/RNCli/constants/style.js
@@ -2,5 +2,16 @@ import {StyleSheet} from 'react-native';
 
 
 export default StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    fontSize: 32,
+    color: 'black',
+  },
   
 });
